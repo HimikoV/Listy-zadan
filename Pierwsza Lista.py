3a7f90100ff4cc33ebe4ad6f326b153a6a9bc086
HimikoV <48067467+HimikoV@users.noreply.github.com>
2019-02-27 19:59:09 +0100

    lab1
    pierwsza lista zadań

--- Pierwsza Lista.py
+++ Pierwsza Lista.py
@@ -113,3 +113,5 @@ plot(y)
 show()
 #5 upload the solution as a Github repository. I suggest creating a directory for the whole python course and subdirectories lab1, lab2 etc. (0.5p)
 #Ad 5 Hint write in Google "how to create a github repo". There are plenty of tutorials explaining this matter.
+
+#my github repository: https://github.com/HimikoV/Listy-zadan
